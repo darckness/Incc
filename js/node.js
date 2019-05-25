3fcc4a69768493aa6c122554af2df9d4d6d69ddb
David Gaspar <davidgaspar.dev@gmail.com>
2019-05-25 15:02:47 -0300

    Added close event menu

--- js/node.js
+++ js/node.js
@@ -3,16 +3,8 @@
   var nav = $('nav'), menu = $('nav h1'), main = $('main'), open = false;
   var menuItem = document.getElementsByTagName('li');
 
-  for(var i = 0; i < menuItem.length; i++) {
-    menuItem[i].addEventListener('click', function(event) {
-      var thisElement = event.target || event.srcElement;
-      document.getElementsByClassName('content')[0].innerHTML = thisElement.textContent;
-    });
-  }
-
-  menu.on('click', function() {
-
-		open = !open ? true : false;
+  function openOrCloseMenu() {
+    open = !open ? true : false;
 
     nav.toggleClass('menu-active');
     main.toggleClass('menu-active');
@@ -20,7 +12,17 @@
     main.removeClass('menu-hover');
 
     console.log(open);
-  });
+  }
+
+  for(var i = 0; i < menuItem.length; i++) {
+    menuItem[i].addEventListener('click', function(event) {
+      var thisElement = event.target || event.srcElement;
+      document.getElementsByClassName('content')[0].innerHTML = thisElement.textContent;
+      openOrCloseMenu();
+    });
+  }
+
+  menu.on('click', openOrCloseMenu);
 
   menu.hover(
 
